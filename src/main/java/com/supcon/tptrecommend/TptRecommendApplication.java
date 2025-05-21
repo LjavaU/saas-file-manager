1ec14f8270f120afe3b4bb5631e042ec03316cec
陆浩 <luhao1@supcon.com>
2025-05-21 15:06:01 +0800

    feat: 初始化代码仓库

---
+++ src/main/java/com/supcon/tptrecommend/TptRecommendApplication.java
@@ -0,0 +1,11 @@
+package com.supcon.tptrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TptRecommendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TptRecommendApplication.class, args);
+    }
+}
