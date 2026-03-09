305d4ecd85bc9daed2b3e042333a28924f2b6129
陆浩 <luhao1@supcon.com>
2026-03-09 14:49:07 +0800

    feat: init

--- src/main/java/com/supcon/tptrecommend/common/exception/UnsupportedFileTypeException.java
+++
@@ -1,9 +0,0 @@
-package com.supcon.tptrecommend.common.exception;
-
-
-public class UnsupportedFileTypeException  extends RuntimeException {
-
-    public UnsupportedFileTypeException(String message) {
-        super(message);
-    }
-}
